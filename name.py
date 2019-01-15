f25df53b147aa05e54d1018942965e7b51804817
felipejsm <felipejsm41@gmail.com>
2019-01-14 23:14:36 -0200

    first commit

---
+++ name.py
@@ -0,0 +1,8 @@
+name = 'ada lovelace';
+print(name.title());
+print(name.upper());
+print(name.lower());
+print("\tPython")
+espaco_d = "Espaço à direita ";
+print("Espaço à direita ");
+print(espaco_d.rstrip());

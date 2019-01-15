b2f09a1443890a7bfa8d85cb5909bee16f94278e
felipejsm <felipejsm41@gmail.com>
2019-01-14 23:28:56 -0200

    trabalhando com listas parte 1

---
+++ chapter03/bicycles.py
@@ -0,0 +1,8 @@
+bicycles = ['trek', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[0])
+# para acessar o último item de uma lista o pytho oferece o índice -1, que show
+print(bicycles[-1])
+print(bicycles[-2])
+
+print("My first bicycle was a " + bicycles[0].title() + ".")

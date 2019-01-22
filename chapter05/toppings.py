3d266af8232ba77b008b132a61a7c2080dbec7d6
felipejsm <felipejsm41@gmail.com>
2019-01-21 23:23:12 -0200

    for and if and empty lists

--- chapter05/toppings.py
+++ chapter05/toppings.py
@@ -11,3 +11,16 @@ if  'pepperoni' in requested_toppings:
     print("Adding pepperoni")
 if 'extra cheese' in requested_toppings:
     print("Adding extra cheese")
+
+requested_toppings.append('green peppers')
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.") 
+    else:
+         print("Adding " + requested_topping + ".")
+empty_list = []
+
+if empty_list:
+    print("not empty")
+else:
+    print("Empty")
